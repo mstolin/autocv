80cc8614432ef8e5d0c0318803ef850aaffaae1f
Marcel Stolin <kuri-dv@mailbox.org>
2023-02-16 11:10:10 +0100

    Tried to create a CLI tool

--- src/main.go
+++ src/main.go
@@ -1,11 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
 
-struct data {
-
-}
+	"github.com/urfave/cli/v2"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	app := &cli.App{
+		Name:  "autocv",
+		Usage: "this is the usage",
+		Flags: []cli.Flag{
+			&cli.PathFlag{
+				Name:     "data",
+				Aliases:  []string{"d"},
+				Usage:    "Usage of data",
+				Required: true,
+			},
+			&cli.PathFlag{
+				Name:     "template",
+				Aliases:  []string{"t"},
+				Usage:    "Usage of template",
+				Required: true,
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			var dataPath string
+			if cCtx.Path("data") != "" {
+				dataPath = cCtx.Path("data")
+			}
+			fmt.Println("dataPath:", dataPath)
+
+			var templatePath string
+			if cCtx.Path("template") != "" {
+				templatePath = cCtx.Path("template")
+			}
+			fmt.Println("templatePath:", templatePath)
+
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
