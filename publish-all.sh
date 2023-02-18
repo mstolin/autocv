a7ed512799cbcdf4feeb73817643e87e31b28c74
Marcel Stolin <kuri-dv@mailbox.org>
2023-02-18 14:13:03 +0100

    Tried to fix the publish script

--- publish-all.sh
+++ publish-all.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-find . -type f -name "*.tex" -print0 | while read -d $'\0' file
-do
+find . -type f -name "*.tex" | while read file; do
     # Render file to pdf
     echo "Compile $file"
     latexmk -pdf "$file"
